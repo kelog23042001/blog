cfd60997aaeea092c4b635f40972c25ef98a2e86
KeLong <kelong23042001@gmail.com>
2022-06-07 11:54:16 +0700

    s

--- resources/views/user/pages/contact/contact.blade.php
+++ resources/views/user/pages/contact/contact.blade.php
@@ -1,20 +1,26 @@
 @extends('layout')
 @section('content')
+<section id="slider">
+    <!--slider-->
+    @include('user.elements.slider')
+</section>
 <div class="col-sm-3">
     <div class="left-sidebar">
         @include('user.elements.left_sidebar')
+        @include('user.elements.left_sidebar.viewed')
+        @include('user.elements.left_sidebar.wishlist')
     </div>
 </div>
         <div>
 
         <h2 class="title text-center">Liên hệ với chúng tôi</h2>
              @foreach($contact as $key => $val)
-                <div class="row">
+                <div class="col-sm-9 padding-right ">
                     <div class="col-md-12">
 
                         <h4>Thông tin liên hệ</h4>
                         {!!$val->info_contact!!}
-                
+
                             {!!$val->info_fanpage!!}
                         </p>
                     </div>
