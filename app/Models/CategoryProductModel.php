1decfc67069217abeefbffe2e418befc63a11849
khalongtvh <khalongtvh@gmail.com>
2022-12-28 14:15:42 +0700

    đá

--- app/Models/CategoryProductModel.php
+++ app/Models/CategoryProductModel.php
@@ -9,12 +9,13 @@ class CategoryProductModel extends Model
 {
     public $timestamps = false; //set time to false
     protected $fillable = [
-    	'meta_keywords', 'category_name', 'slug_category_product','category_desc','category_status','category_order'
+        'meta_keywords', 'category_name', 'slug_category_product', 'category_desc', 'category_status', 'category_order'
     ];
     protected $primaryKey = 'category_id';
- 	protected $table = 'tbl_category_product';
+    protected $table = 'tbl_category_product';
 
-    public function product(){
+    public function product()
+    {
         return $this->hasMany('App\Models\Product');
     }
 }
