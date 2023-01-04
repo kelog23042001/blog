bc8e48cbc6bef4f6ad70acd1f2ac39ba33225953
khalongtvh <khalongtvh@gmail.com>
2023-01-04 09:50:23 +0700

    asd

--- config/mail.php
+++ config/mail.php
@@ -37,7 +37,8 @@ return [
         'smtp' => [
             'transport' => 'smtp',
             'host' => env('MAIL_HOST', 'smtp.gmail.com'),
-            'port' => env('MAIL_PORT', 465),
+            'port' => env('MAIL_PORT', 587),
+            // 'port' => env('MAIL_PORT', 465),
             'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
@@ -82,9 +83,9 @@ return [
 
     'stream'  => [
         'ssl'  => [
-        'allow_self_signed'  => true,
-        'verify_peer'  => false,
-        'verify_peer_name'  => false,
+            'allow_self_signed'  => true,
+            'verify_peer'  => false,
+            'verify_peer_name'  => false,
         ],
     ],
     /*
